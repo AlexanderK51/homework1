4bb0fbed6c74f042831a52282b47468bd4bf0622
AlexanderK <askotsur@yandex.ru>
2024-10-06 16:58:26 +0000

    new vm

--- build/CMakeFiles/hello_world.dir/DependInfo.cmake
+++ build/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -8,16 +8,12 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/smuser/homework1/homework1/function.cpp" "CMakeFiles/hello_world.dir/function.cpp.o" "gcc" "CMakeFiles/hello_world.dir/function.cpp.o.d"
-  "/home/smuser/homework1/homework1/hello_world.cpp" "CMakeFiles/hello_world.dir/hello_world.cpp.o" "gcc" "CMakeFiles/hello_world.dir/hello_world.cpp.o.d"
+  "/home/smuser/homework1/function.cpp" "CMakeFiles/hello_world.dir/function.cpp.o" "gcc" "CMakeFiles/hello_world.dir/function.cpp.o.d"
+  "/home/smuser/homework1/hello_world.cpp" "CMakeFiles/hello_world.dir/hello_world.cpp.o" "gcc" "CMakeFiles/hello_world.dir/hello_world.cpp.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/smuser/homework1/homework1
+# Install script for directory: /home/smuser/homework1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/smuser/homework1/homework1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/smuser/homework1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
